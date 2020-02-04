4aaabc836a177739e0583e8bb10f02c07214ea65
mike g <mike@mikes-MacBook-Pro.local>
2020-02-03 22:10:31 -0500

    update cloudfront instances

--- main.tf
+++ main.tf
@@ -383,51 +383,6 @@ resource "aws_cloudfront_distribution" "podcast_content" {
     max_ttl                = 86400
   }
 
-//  # Cache behavior with precedence 0
-//  ordered_cache_behavior {
-//    path_pattern     = "/content/immutable/*"
-//    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-//    cached_methods   = ["GET", "HEAD", "OPTIONS"]
-//    target_origin_id = local.s3_origin_id
-//
-//    forwarded_values {
-//      query_string = false
-//      headers      = ["Origin"]
-//
-//      cookies {
-//        forward = "none"
-//      }
-//    }
-//
-//    min_ttl                = 0
-//    default_ttl            = 86400
-//    max_ttl                = 31536000
-//    compress               = true
-//    viewer_protocol_policy = "redirect-to-https"
-//  }
-//
-//  # Cache behavior with precedence 1
-//  ordered_cache_behavior {
-//    path_pattern     = "/content/*"
-//    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-//    cached_methods   = ["GET", "HEAD"]
-//    target_origin_id = local.s3_origin_id
-//
-//    forwarded_values {
-//      query_string = false
-//
-//      cookies {
-//        forward = "none"
-//      }
-//    }
-//
-//    min_ttl                = 0
-//    default_ttl            = 3600
-//    max_ttl                = 86400
-//    compress               = true
-//    viewer_protocol_policy = "redirect-to-https"
-//  }
-
   price_class = "PriceClass_200"
   # Update locations later
   restrictions {
@@ -490,51 +445,6 @@ resource "aws_cloudfront_distribution" "podcast_rss" {
     default_ttl            = 3600
     max_ttl                = 86400
   }
-
-//  # Cache behavior with precedence 0
-//  ordered_cache_behavior {
-//    path_pattern     = "/content/immutable/*"
-//    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-//    cached_methods   = ["GET", "HEAD", "OPTIONS"]
-//    target_origin_id = local.s3_origin_id
-//
-//    forwarded_values {
-//      query_string = false
-//      headers      = ["Origin"]
-//
-//      cookies {
-//        forward = "none"
-//      }
-//    }
-//
-//    min_ttl                = 0
-//    default_ttl            = 86400
-//    max_ttl                = 31536000
-//    compress               = true
-//    viewer_protocol_policy = "redirect-to-https"
-//  }
-//
-//  # Cache behavior with precedence 1
-//  ordered_cache_behavior {
-//    path_pattern     = "/content/*"
-//    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-//    cached_methods   = ["GET", "HEAD"]
-//    target_origin_id = local.s3_origin_id
-//
-//    forwarded_values {
-//      query_string = false
-//
-//      cookies {
-//        forward = "none"
-//      }
-//    }
-//
-//    min_ttl                = 0
-//    default_ttl            = 3600
-//    max_ttl                = 86400
-//    compress               = true
-//    viewer_protocol_policy = "redirect-to-https"
-//  }
 
   price_class = "PriceClass_200"
   # Update locations
