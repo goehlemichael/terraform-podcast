d9563f3055f25c2e1d496f0c1d271f6f447d4570
mike g <goehlemichael@hotmail.com>
2021-08-27 09:37:08 -0400

    upgrade archive version

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     archive = {
       source  = "hashicorp/archive"
-      version = "2.0.0"
+      version = "2.2.0"
     }
     aws = {
       source  = "hashicorp/aws"
