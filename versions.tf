287c8043210c6a6ba91160d68feb358e823656b1
mike g <goehlemichael@hotmail.com>
2021-08-28 08:47:45 -0400

    pin the versions

--- versions.tf
+++ versions.tf
@@ -2,17 +2,18 @@ terraform {
   required_providers {
     archive = {
       source  = "hashicorp/archive"
-      version = "2.2.0"
+      version = "~> 2.2.0"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 3.56.0"
     }
   }
-  required_version = ">= 0.13"
+  required_version = "~> 1.0.5"
 }
 
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
+//  profile = ""
 }
