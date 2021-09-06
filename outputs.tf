e3ff8849d40c0261be700743c40853816ce186d9
mike g <goehlemichael@hotmail.com>
2021-09-06 15:18:34 -0400

    add outputs, update variable desciptions, add check for aws region set

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,38 @@
 output "podcast_url" {
   value       = aws_lambda_function.podcast_xml_generator.environment[0].variables.podcast_url
-  description = "This is the url to reach the rss feed for the podcast"
+  description = "url to reach the rss feed for the podcast"
 }
 
 output "podcast_feed_cdn_url" {
   value       = aws_cloudfront_distribution.podcast_rss.domain_name
-  description = "This is the url to reach the rss feed for the podcast"
+  description = "url to reach the rss feed for the podcast"
 }
 
 output "content_bucket_url" {
-  value = aws_cloudfront_distribution.podcast_content.domain_name
-  description = "The s3 url for the content bucket, should not be accessible"
+  value = aws_s3_bucket.content.bucket_domain_name
+  description = "only root url should be accessible"
+}
+
+output "content_bucket_name" {
+  value = aws_s3_bucket.content.bucket
+  description = "for testing bucket"
 }
 
 output "log_bucket_url" {
   value = aws_s3_bucket.logs.bucket_domain_name
   description = "The s3 url for the content bucket, should not be accessible"
+}
+
+output "content_cdn_url" {
+  value = aws_cloudfront_distribution.podcast_content.domain_name
+  description = "The s3 url for the content bucket, should not be accessible"
+}
+
+output "lambda_name" {
+  value = aws_lambda_function.podcast_xml_generator.function_name
+  description = "The name of the lambda function"
+}
+
+output "region" {
+  value = data.aws_region.current.name
 }
