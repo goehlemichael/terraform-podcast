4bdc2d3221f8eb1173670cc3f4a45c94ae4db182
mike g <goehlemichael@hotmail.com>
2022-02-20 12:19:27 -0500

    reorginazation, creating module

--- modules/aws_podcast/outputs.tf
+++ modules/aws_podcast/outputs.tf
@@ -9,27 +9,27 @@ output "podcast_feed_cdn_url" {
 }
 
 output "content_bucket_url" {
-  value = aws_s3_bucket.content.bucket_domain_name
+  value       = aws_s3_bucket.content.bucket_domain_name
   description = "only root url should be accessible"
 }
 
 output "content_bucket_name" {
-  value = aws_s3_bucket.content.bucket
+  value       = aws_s3_bucket.content.bucket
   description = "for testing bucket"
 }
 
 output "log_bucket_url" {
-  value = aws_s3_bucket.logs.bucket_domain_name
+  value       = aws_s3_bucket.logs.bucket_domain_name
   description = "The s3 url for the content bucket, should not be accessible"
 }
 
 output "content_cdn_url" {
-  value = aws_cloudfront_distribution.podcast_content.domain_name
+  value       = aws_cloudfront_distribution.podcast_content.domain_name
   description = "The s3 url for the content bucket, should not be accessible"
 }
 
 output "lambda_name" {
-  value = aws_lambda_function.podcast_xml_generator.function_name
+  value       = aws_lambda_function.podcast_xml_generator.function_name
   description = "The name of the lambda function"
 }
 
